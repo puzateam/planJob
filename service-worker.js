a47cf2f7f9a1be210d6a98b2d52c859444a44b7a
puzateam <suchat2520@gmail.com>
2025-06-18 16:52:30 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,57 @@
+const CACHE_NAME = 'planjob-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
 
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+    // เราจะไม่ cache การเรียก API
+    if (event.request.url.includes('macros.google.com')) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+    
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            // Cache hit - return response
+            if (response) {
+            return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
